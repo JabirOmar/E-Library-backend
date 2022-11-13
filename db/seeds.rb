3e948e6f83d3cb4b3a81162ebfc872286efba630
Terormumuli <terahmumuli@gmail.com>
2022-11-13 11:16:25 +0300

    added some dependencies in the seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,11 +21,11 @@ end
 end
 
 
-5.times do
-    seeded_reviews = Review.create(
-        comments:Faker::Verb.past_participle
-    )
-end
+review1 = Review.create(comments:"I liked this less than Divergent but it was exciting and fun!")
+review2 = Review.create(comments:"I love the strong female protagonist and the quick and exciting story.")
+review3 = Review.create(comments:"This book shattered my heart to pieces but I loved it all the same.")
+review4 = Review.create(comments:"The story is amazing but so incredibly slow, to be honest I enjoyed the movie more!")
+review5 = Review.create(comments:"More fun characters are introduced which made this book interesting, besides that I don't remember much abiut the story.")
 
 
 puts "✅ Done seeding!"
